c0b152c11c4dc454d78a1415fb0810438d16562f
drissireda <mohamedreda.drissi@edu.uca.ma>
2018-02-13 19:47:44 +0100

    new take on the coloration

---
+++ maxc.cpp
@@ -0,0 +1,74 @@
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string.h>
+#include <map>
+#include <assert.h>
+#include "maxc.h"
+
+using namespace std;
+
+void read_dimacs(string name, bool** &conn, int &size) {
+    ifstream f (name.c_str());
+    string buffer;
+    assert(f.is_open());
+    set<int> v;
+    multimap<int,int> e;
+    while (!getline(f, buffer).eof()) {
+        int vi, vj;
+        sscanf(buffer.c_str(), "%d %d", &vi, &vj);
+        v.insert(vi);
+        v.insert(vj);
+        e.insert(make_pair(vi, vj));
+    }
+//  size = v.size() + 1;
+    size = *v.rbegin() + 1;
+    conn = new bool*[size];
+    for (int i=0; i < size; i++) {
+        conn[i] = new bool[size];
+        memset(conn[i], 0, size * sizeof(bool));
+    }
+    for (multimap<int,int>::iterator it = e.begin(); it != e.end(); it++) {
+        conn[it->first][it->second] = true;
+        conn[it->second][it->first] = true;
+    }
+    cout << "|E| = " << e.size() << "  |V| = " << v.size() << " p = " << (double) e.size() / (v.size() * (v.size() - 1) / 2) << endl;
+    f.close();
+}
+
+
+int main(int argc, char *argv[]) {
+    assert(argc == 2);
+    cout << "args = " << argv[1] << endl;
+    bool **conn;
+    int size;
+    read_dimacs(argv[1], conn, size);
+    cout << "max clique with improved coloring"<<endl;
+    Maxclique m(conn, size);
+    int *qmax;
+    int qsize;
+    m.mcq(qmax, qsize);  // run max clique with improved coloring
+    cout << "Maximum clique: ";
+    for (int i = 0; i < qsize; i++)
+        cout << qmax[i] << " ";
+    cout << endl;
+    cout << "Size = " << qsize << endl;
+    cout << "Number of steps = " << m.steps() << endl;
+    delete [] qmax;
+    cout << "max clique with improved coloring and dynamic sorting of vertices"<<endl;
+    Maxclique md(conn, size, 0.025);  //(3rd parameter is optional - default is 0.025 - this heuristics parameter enables you to use dynamic resorting of vertices (time expensive)
+    // on the part of the search tree that is close to the root - in this case, approximately 2.5% of the search tree -
+    // you can probably find a more optimal value for your graphs
+    md.mcqdyn(qmax, qsize);  // run max clique with improved coloring and dynamic sorting of vertices
+    cout << "Maximum clique: ";
+    for (int i = 0; i < qsize; i++)
+        cout << qmax[i] << " ";
+    cout << endl;
+    cout << "Size = " << qsize << endl;
+    cout << "Number of steps = " << md.steps() << endl;
+    delete [] qmax;
+    for (int i=0; i<size; i++)
+        delete [] conn[i];
+    delete [] conn;
+    return 0;
+}
